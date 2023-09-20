174d59cb170739311f975099646c75fb300be191
Karchkov Denis <karchkov.denis@mail.ru>
2023-09-20 10:03:31 +0300

    Append tests

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <assert.h>
+
+int intro_sum(int a, int b){
+    return a + b;
+}
 
 int main(void){
-    printf("Hello world %d", 42);
+    assert(intro_sum(1,2) == 3);
+    assert(intro_sum(100, 200) == 100);
 }
